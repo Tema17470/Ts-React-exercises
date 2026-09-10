1f818cd345ff099456af65739de5e6a0c850baf9
Tema17470 <lanenkov04@gmail.com>
2026-09-10 14:15:41 +0300

    basic server was set up

--- testapp/src/App.tsx
+++ testapp/src/App.tsx
@@ -6,40 +6,7 @@ import './App.css'
 import {handleClick1,handleClick2,handleClick3} from './components/functions1.tsx';
 import Note from './components/Notes.tsx'
 
-/* cheking importing functionality 
-let result: number;
-
-function sum (p1: number, p2: number){
-  console.log("Function sum:")
-  console.log(p1)
-  console.log(p2)
-  return p1 + p2
-}
-
-function handleClick1() {
-  result = sum(5, 9);
-  console.log(result);
-}
-
-function product (p1: number, p2: number){
-  console.log("Function product:")
-  console.log(p1)
-  console.log(p2)
-  return p1 * p2
-}
-
-function handleClick2() {
-  result = product(5, 9);
-  console.log(result);
-}
-
-function substraction (p1: number, p2: number){
-  console.log("Function substraction:")
-  console.log(p1)
-  console.log(p2)
-  return p1 - p2
-}
-
+/* now is imported
 function handleClick3() {
   result = substraction(5, 9);
   console.log(result);
