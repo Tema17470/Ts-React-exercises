ab358ecdf6a4f2de5fe7be397fafb3f3734a50a2
Tema17470 <lanenkov04@gmail.com>
2026-09-15 11:17:31 +0300

    Added: simple backend server Modified: moved old backend from 3001 to 3002

--- testapp/src/App.tsx
+++ testapp/src/App.tsx
@@ -108,7 +108,7 @@ function App() {
   // Note importance switch
 
   const toggleImportanceOf = id => {
-    const url = `http://localhost:3001/notes/${id}`
+    const url = `http://localhost:3002/notes/${id}`
     const note = notes.find(n => n.id === id)
     const changedNote = { ...note, important: !note.important }
 
