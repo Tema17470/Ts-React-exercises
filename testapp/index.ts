c75694870b404f92417e6668681c4649514ac514
Tema17470 <lanenkov04@gmail.com>
2026-09-15 16:04:54 +0300

    Added: posting and deleting content on the server

--- testapp/index.ts
+++ testapp/index.ts
@@ -1,5 +1,8 @@
 import express from 'express'
+
 const app = express()
+
+app.use(express.json())
 
 let notes = [
   {
@@ -36,6 +39,40 @@ app.get('/api/notes/:id', (request, response) => {
   } else {
     response.status(404).end()
   }
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = request.params.id
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
+// function for propper id generation
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => Number(n.id)))
+    : 0
+  return String(maxId + 1)
+}
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+
+  response.json(note)
 })
 
 const PORT = 3001
